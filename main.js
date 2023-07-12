346a6c419691c0b8c5e54d1d65fad6def1b02773
Vincent Riva <vincent.riva@outlook.fr>
2023-07-12 21:07:15 +0200

    ✨ Add basic ZIP compression & configuration

---
+++ main.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const fs = require('fs');
+const archiver = require('archiver');
+const args = require('yargs').argv;
+const { glob, globSync, globStream, globStreamSync, Glob } = require('glob');
+
+const config = {
+  dirName: null,
+  pathDirectoryToZip: null,
+  ignoreFiles: [],
+};
+
+const checkArguments = () => {
+  return new Promise((resolve, reject) => {
+    if (args._.length !== 1) {
+      reject('You can only specify 1 directory.');
+    }
+
+    const directoryToZip = args._[0];
+    config.pathDirectoryToZip = path.resolve(__dirname, directoryToZip);
+
+    const dirName = path.dirname(config.pathDirectoryToZip).split(path.sep).pop();
+    config.dirName = dirName;
+
+    resolve();
+  });
+};
+
+const buildFromConfigFile = () => {
+  return new Promise((resolve, reject) => {
+    const pathConfigFile = path.resolve(__dirname, '.stockrc');
+    const configFile = fs.readFileSync(pathConfigFile, 'utf-8');
+    config.ignoreFiles = configFile.split('\r\n');
+
+    resolve();
+  });
+};
+
+const gatherFiles = () => {
+  return new Promise((resolve, reject) => {
+    const output = fs.createWriteStream(__dirname + `/${config.dirName}.zip`);
+    const archive = archiver('zip', {
+      zlib: { level: 9 }, // Sets the compression level.
+    });
+
+    output.on('close', function () {
+      console.log(archive.pointer() + ' total bytes');
+      console.log('archiver has been finalized and the output file descriptor has closed.');
+      resolve();
+    });
+    output.on('end', function () {
+      console.log('Data has been drained');
+      resolve();
+    });
+    archive.on('error', function (err) {
+      reject(err);
+    });
+    archive.pipe(output);
+
+    archive.glob(config.pathDirectoryToZip, { pattern: '**/*.*', skip: config.ignoreFiles, cwd: config.pathDirectoryToZip });
+    archive.finalize();
+  });
+};
+
+checkArguments()
+  .then(buildFromConfigFile)
+  .then(gatherFiles)
+  .then(() => {
+    console.log('Success');
+  })
+  .catch((err) => {
+    throw new Error(err);
+  });
