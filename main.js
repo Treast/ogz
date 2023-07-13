06b89e882b4a0d88817e26f9462be63d951aacab
Vincent Riva <vincent.riva@outlook.fr>
2023-07-13 10:36:49 +0200

    ✨ Add Ora spinner & add yargs configuration

--- main.js
+++ main.js
@@ -1,18 +1,55 @@
-const path = require('path');
-const fs = require('fs');
-const archiver = require('archiver');
-const { cwd } = require('process');
-const args = require('yargs').argv;
+import path from 'path';
+import fs from 'fs';
+import archiver from 'archiver';
+import { cwd } from 'process';
+import ora from 'ora';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const args = yargs(hideBin(process.argv))
+  .options({
+    recursive: {
+      alias: 'r',
+      boolean: true,
+      describe: 'Run the program recursively, archiving subfolders of the specified folders',
+    },
+    format: {
+      alias: 'f',
+      describe: 'Choose between ZIP or TAR archiving format',
+      default: 'zip',
+    },
+  })
+  .parse();
 
 const config = {
   projects: [],
   ignoreFiles: [],
   format: 'zip',
+  spinner: null,
 };
 
 const checkArguments = () => {
   return new Promise((resolve, reject) => {
-    args._.map((project) => {
+    let directories = args._;
+    config.spinner = ora('Computing folders').start();
+
+    if (args.r || args.recursive) {
+      directories = directories
+        .map((directory) => {
+          const directoryPath = path.resolve(__dirname, directory);
+          return fs
+            .readdirSync(directoryPath, { withFileTypes: true })
+            .filter((dirent) => dirent.isDirectory())
+            .map((dir) => path.resolve(directoryPath, dir.name));
+        })
+        .flat();
+    }
+
+    directories.map((project) => {
       const pathDirectoryToZip = path.resolve(__dirname, project);
       const dirName = pathDirectoryToZip.split(path.sep).pop();
       config.projects.push({
@@ -21,7 +58,7 @@ const checkArguments = () => {
       });
     });
 
-    if (args.tar) {
+    if (args.f === 'tar' || args.format === 'tar') {
       config.format = 'tar';
     }
 
@@ -31,6 +68,9 @@ const checkArguments = () => {
 
 const buildFromConfigFile = () => {
   return new Promise((resolve, reject) => {
+    config.spinner.succeed();
+    config.spinner = ora('Loading configuration file').start();
+
     let pathConfigFile = path.resolve(__dirname, '.stockrc');
 
     const localConfigFile = path.resolve(cwd(), '.stockrc');
@@ -47,6 +87,7 @@ const buildFromConfigFile = () => {
 };
 
 const zipProjects = () => {
+  config.spinner.succeed();
   const promises = config.projects.map((project) => {
     return gatherFiles(project);
   });
@@ -54,8 +95,21 @@ const zipProjects = () => {
   return Promise.all(promises);
 };
 
+const units = ['bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+const convertUnits = (bytes) => {
+  let l = 0,
+    n = parseInt(bytes, 10) || 0;
+
+  while (n >= 1024 && ++l) {
+    n = n / 1024;
+  }
+
+  return n.toFixed(n < 10 && l > 0 ? 1 : 0) + ' ' + units[l];
+};
+
 const gatherFiles = (project) => {
   return new Promise((resolve, reject) => {
+    project.spinner = ora(`Archiving ${project.dirName}.${config.format}`).start();
     const output = fs.createWriteStream(cwd() + `/${project.dirName}.${config.format}`);
     const archive = archiver(config.format, {
       zlib: { level: 9 }, // Sets the compression level.
@@ -63,17 +117,19 @@ const gatherFiles = (project) => {
     });
 
     output.on('close', function () {
-      console.log(archive.pointer() + ' total bytes');
-      console.log('archiver has been finalized and the output file descriptor has closed.');
+      project.spinner.succeed(`Archiving ${project.dirName}.${config.format} (${convertUnits(archive.pointer())} bytes)`);
       resolve();
     });
+
     output.on('end', function () {
-      console.log('Data has been drained');
       resolve();
     });
+
     archive.on('error', function (err) {
+      project.spinner.fail();
       reject(err);
     });
+
     archive.pipe(output);
 
     archive.glob(project.pathDirectoryToZip, {
@@ -94,7 +150,8 @@ checkArguments()
   .then(zipProjects)
   .then(() => {
     // console.log(config);
-    console.log('Success');
+    // console.log('Success');
+    config.spinner = ora('Finished !').start().succeed();
   })
   .catch((err) => {
     throw new Error(err);
